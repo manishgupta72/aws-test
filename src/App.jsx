d59d3389bbade905753564ddd68f7b18b8ebb14d
Manish Gupta <manishgupta724872@gmail.com>
2025-01-25 19:42:26 +0530

    build updated with some change

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter,Routes,Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Login from './pages/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <Link to='/login'>Login</Link>
-    <h1>Hello Welcome to Aws testing</h1>
-    <h2>Now update reflected on the aws</h2>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/login' element={<Login/>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+      <h1>Welcome to the aws testing</h1>
+        <nav>
+          <Link to='/login'>Login</Link>
+        </nav>
+        <Routes>
+          {/* Route for the Home component can be added here */}
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
